6c284ff5c396835ae03769b81c27b5b3ff5d32b3
GabrielSilvaData <gabrielsilva2822@gmail.com>
2026-04-02 21:24:39 -0300

    Exercício feito

---
+++ atividade2.py
@@ -0,0 +1,7 @@
+Número = int(input('Digite um número:'))
+Dobro = Número*2
+Triplo = Número*3
+Quadrado = Número*Número
+print (f'O dobro é: {Dobro}')
+print (f'O triplo é: {Triplo}')
+print (f'O quadrado é: {Quadrado}')
