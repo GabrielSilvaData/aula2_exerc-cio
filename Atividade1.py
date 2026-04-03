2122aaeb0f91384d87d60b0a7f31d49ec1e7ef0c
GabrielSilvaData <gabrielsilva2822@gmail.com>
2026-04-02 21:06:34 -0300

    inicializa o repositório

---
+++ Atividade1.py
@@ -0,0 +1,17 @@
+# Número = int (input('Informe um número:'))
+# Posterior = Número +1
+# Anterior = Número -1
+
+# print(f'O número é:{Número}')
+# print(f'O número posterior é:{Posterior}')
+# print(f'O número anterior é:{Anterior}')
+
+#--------------------------------------------------
+
+#Aumento Salarial
+
+Salário = float (input('Informe o seu salário: '))
+Adcional = Salário* 0.15
+Novo_Salário = Salário + Adcional
+print(f'O aumento foi de: {Adcional:.2f}')
+print(f'O novo salário é: {Novo_Salário:.2f}')
---
+++ exemplo1.py
@@ -0,0 +1,63 @@
+#nome = 'Maria'
+#idade = 18
+
+#print(f'Olá {nome}')
+#print(f'Você tem {idade} anos')
+
+#--------------------------------------
+
+# Atividade Fixação
+# nome = 'João'
+# bairro = 'Neves'
+# profissão = 'Analista de Dados'
+
+# print(f'Olá {nome}')
+# print(f'Você Mora em {bairro}')
+# print(f'Você trabalha como {profissão}')
+
+#------------------------------------------------
+#Operadores
+#preco = 20
+#quantidade = 3
+#Total = preco * quantidade
+#print(f'O total é {Total}')
+
+#-------------------------------------------------
+# salário = 2000
+# bônus = 980
+# salário_total = salário + bônus
+# print(f'O Salário total é {salário_total}')
+
+# Exemplo de entrada de valores pelo usuário
+# nome = input('Informe o nome:')
+# print(f'Olá {nome}!')
+
+#-------------------------------------------------
+#ATIVIDADE FIXAÇÃO
+#-------------------------------------------------
+# salário = float(input('Informe o seu salário:'))
+# bônus = float(input('Informe o seu bônus:'))
+# salário_total = salário + bônus
+# print(f'O seu salário é: {salário_total}')
+
+#string = texto
+#float = Números decimais 
+#int = Número inteiro
+#Boleano = True ou False
+
+#Cálculo de Porcentagem
+
+preço = float(input('Informe o preço: '))
+quantidade = int(input('Informe a quantidade: '))
+total = preço * quantidade
+desconto = total *0.1 #10% - Isso é o mesmo que 10 / 100
+Valor_final = total - desconto
+print(f'O Valor a pagar: {Valor_final}')
+print(f'O Valor do desconto é: {desconto} ')
+
+
+
+
+
+
+
